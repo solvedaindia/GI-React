d9b4f751cbed84f55387fe511e2dfdc5b336151e
Mritunjay <mkumar@solveda.com>
2019-03-27 16:48:27 +0530

    PLP Products Item UI Implemented

--- app/src/components/GlobalComponents/productItem/productItem.js
+++ app/src/components/GlobalComponents/productItem/productItem.js
@@ -5,43 +5,27 @@ import Price from './price';
 import Promotions from './promotion';
 
 class ProductItem extends React.Component {
-
-    render() {
-        console.log('myyyyy---', this.props.data);
-        return (
-            <div className='col-xl-4 col-lg-4 col-md-4 col-sm-6'>
-                <div className='productBox'>
-                    <div className='imgBox'>
-                        <ItemImage data={this.props.data.imageSrc} />
-                        <div className='featurepro-like'>
-                            <div className='fa fa-heart-o wishlist'></div>
-                        </div>
-                        <div className='overlay-comparequick'>
-                            <div className='inner-overlay'>
-                                <ul className='colortheme clearfix'>
-                                    <li className='colortheme-list yellow'></li>
-                                    <li className='colortheme-list red-dirt'></li>
-                                    <li className='colortheme-list grey'></li>
-                                    <li className='colortheme-list blue'></li>
-                                </ul>
-                                <div className='btn-wrapper'>
-                                    <button className='btnborder-line'>Compare +</button>
-                                    <button className='btnborder-line'>Quick View</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <RibbonTag data={this.props.data.ribbonText}/>
-                    <div className='product-text'>
-                        <p className='paragraph'>{this.props.data.productName}</p>
-                        <Price actualPrice={this.props.data.actualPrice} offerPrice={this.props.data.offerPrice}/>
-                        <Promotions data={this.props.data.promotionData}/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    console.log('myyyyy---', this.props.data);
+    return (
+      <li className="col-xl-4 col-lg-4 col-md-4 col-sm-6 productlist">
+        <div className="imgBox">
+          <ItemImage data={this.props.data.imageSrc} />
+          <button className="btn-outofStock">Out of Stock</button>
+        </div>
+        <RibbonTag data={this.props.data.ribbonText} />
+        <div className="product-text">
+          <p className="heading text">{this.props.data.productName}</p>
+          <p className="price text">
+            {/* <Price actualPrice={this.props.data.actualPrice} offerPrice={this.props.data.offerPrice}/> */}
+            <span className="discount-price"> 24,700</span>{' '}
+            <span className="priceno-discount">26,000</span>
+          </p>
+          <Promotions data={this.props.data.promotionData} />
+        </div>
+      </li>
+    );
+  }
 }
 
 export default ProductItem;
