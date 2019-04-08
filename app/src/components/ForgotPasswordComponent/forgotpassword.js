ecb448330d65621c40ead4bf9be6489722d847fd
Dimple Jaiswal <djaiswal@salmonggn.com>
2019-04-08 11:20:03 +0530

    Login screen changes

--- app/src/components/ForgotPasswordComponent/forgotpassword.js
+++ app/src/components/ForgotPasswordComponent/forgotpassword.js
@@ -17,145 +17,145 @@ const prevArrowImg = (
   />
 );
 class Forgotpassowrd extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false,
-      currentItem: null,
-      currentItemName: null,
-      userId: null,
-      otpNo: null,
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+		modal: false,
+		currentItem: null,
+		currentItemName: null,
+		userId: null,
+		otpNo: null,
+		};
 
-    this.toggle = this.toggle.bind(this);
-    this.handler = this.handler.bind(this);
-  }
+		this.toggle = this.toggle.bind(this);
+		this.handler = this.handler.bind(this);
+	}
 
-  handler(itemStr, userId, otpStr) {
-    if (userId != null) {
-      this.setState({
-        userId,
-      });
-    }
+	handler(itemStr, userId, otpStr) {
+		if (userId != null) {
+		this.setState({
+			userId,
+		});
+		}
 
-    console.log('ComponentName---', itemStr);
-    let item;
-    if (itemStr == 'ForgotPasswordOTP') {
-      item = (
-        <ForgotPasswordOTP
-          handlerPro={this.handler.bind(this)}
-          userIdPro={this.state.userId}
-          isHeadingPro
-          isBack={false}
-        />
-      );
-    } else if (itemStr == 'ForgotPasswordNewPassword') {
-      item = (
-        <ForgotPasswordNewPassword
-          handlerPro={this.handler.bind(this)}
-          userIdPro={this.state.userId}
-          otpPro={otpStr}
-          isBack={false}
-        />
-      );
-    } else if (itemStr == 'NewPasswordSuccess') {
-      this.setState({
-        modal: false,
-      });
-    }
+		console.log('ComponentName---', itemStr);
+		let item;
+		if (itemStr == 'ForgotPasswordOTP') {
+		item = (
+			<ForgotPasswordOTP
+			handlerPro={this.handler.bind(this)}
+			userIdPro={this.state.userId}
+			isHeadingPro
+			isBack={false}
+			/>
+		);
+		} else if (itemStr == 'ForgotPasswordNewPassword') {
+		item = (
+			<ForgotPasswordNewPassword
+			handlerPro={this.handler.bind(this)}
+			userIdPro={this.state.userId}
+			otpPro={otpStr}
+			isBack={false}
+			/>
+		);
+		} else if (itemStr == 'NewPasswordSuccess') {
+		this.setState({
+			modal: false,
+		});
+		}
 
-    this.setState({
-      currentItem: item,
-      currentItemName: itemStr,
-    });
-  }
+		this.setState({
+		currentItem: item,
+		currentItemName: itemStr,
+		});
+	}
 
-  toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal,
-    }));
+	toggle() {
+		this.setState(prevState => ({
+		modal: !prevState.modal,
+		}));
 
-    if (this.state.modal) {
-      this.setState({
-        currentItem: null,
-      });
-    }
-  }
+		if (this.state.modal) {
+		this.setState({
+			currentItem: null,
+		});
+		}
+	}
 
-  backButtonPressed() {
-    let itemStr = this.state.currentItemName;
-    let item;
-    console.log('ComponentNameBAck---', this.state.currentItemName);
-    if (itemStr == 'ForgotPasswordOTP') {
-      item = (
-        <ForgotPasswordEmailMobile
-          handlerPro={this.handler.bind(this)}
-          userIdPro={this.state.userId}
-          isBack
-        />
-      );
-    } else if (itemStr == 'ForgotPasswordNewPassword') {
-      item = (
-        <ForgotPasswordOTP
-          handlerPro={this.handler.bind(this)}
-          userIdPro={this.state.userId}
-          isHeadingPro
-          isBack
-        />
-      );
-      itemStr = 'ForgotPasswordOTP';
-    } else if (itemStr == 'NewPasswordSuccess') {
-      this.setState({
-        modal: false,
-      });
-    }
+	backButtonPressed() {
+		let itemStr = this.state.currentItemName;
+		let item;
+		console.log('ComponentNameBAck---', this.state.currentItemName);
+		if (itemStr == 'ForgotPasswordOTP') {
+		item = (
+			<ForgotPasswordEmailMobile
+			handlerPro={this.handler.bind(this)}
+			userIdPro={this.state.userId}
+			isBack
+			/>
+		);
+		} else if (itemStr == 'ForgotPasswordNewPassword') {
+		item = (
+			<ForgotPasswordOTP
+			handlerPro={this.handler.bind(this)}
+			userIdPro={this.state.userId}
+			isHeadingPro
+			isBack
+			/>
+		);
+		itemStr = 'ForgotPasswordOTP';
+		} else if (itemStr == 'NewPasswordSuccess') {
+		this.setState({
+			modal: false,
+		});
+		}
 
-    this.setState({
-      currentItem: item,
-      currentItemName: itemStr,
-    });
+		this.setState({
+		currentItem: item,
+		currentItemName: itemStr,
+		});
 
-    console.log('Back');
-  }
+		console.log('Back');
+	}
 
-  render() {
-    let item;
-    if (this.state.currentItem == null) {
-      item = (
-        <ForgotPasswordEmailMobile
-          handlerPro={this.handler.bind(this)}
-          isBack={false}
-        />
-      );
-    } else {
-      item = this.state.currentItem;
-    }
+	render() {
+		let item;
+		if (this.state.currentItem == null) {
+		item = (
+			<ForgotPasswordEmailMobile
+			handlerPro={this.handler.bind(this)}
+			isBack={false}
+			/>
+		);
+		} else {
+		item = this.state.currentItem;
+		}
 
-    return (
-      <>
-        <Button className="btn-bg" onClick={this.toggle}>
-          Forgot Password
-        </Button>
-        <Modal show={this.state.modal} onHide={this.toggle}>
-          <Modal.Body>
-            <div className="modal-wrapper sliderContainer">
-              <Button className="close" onClick={this.toggle}>
-                X
-              </Button>
-              <Button
-                onClick={this.backButtonPressed.bind(this)}
-                className="btn-back"
-              >
-                {' '}
-                Back
-              </Button>
-              <div className="form-center">{item}</div>
-            </div>
-          </Modal.Body>
-        </Modal>
-      </>
-    );
-  }
+		return (
+		<>
+			<p className="forgotPassword" onClick={this.toggle}>
+			Forgot Password?
+			</p>
+			<Modal show={this.state.modal} onHide={this.toggle}>
+				<Modal.Body>
+					<div className="modal-wrapper sliderContainer">
+						<Button className="close" onClick={this.toggle}>
+							X
+						</Button>
+						<Button
+							onClick={this.backButtonPressed.bind(this)}
+							className="btn-back"
+						>
+							{' '}
+							Back
+						</Button>
+						<div className="form-center">{item}</div>
+					</div>
+				</Modal.Body>
+			</Modal>
+		</>
+		);
+	}
 }
 
 Forgotpassowrd.propTypes = {};
--- app/src/components/WelcomeBack/index.js
+++ app/src/components/WelcomeBack/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 import FacebookLogin from 'react-facebook-login';
 import GoogleLogin from 'react-google-login';
+import WhiteLogo from '../SVGs/whiteLogo';
 import axios from 'axios';
 import WelcomeBackForm from '../WelcomeBackForm';
+import JoinUs from '../RegisterComponent/joinUs';
 import {
   facebookAppId,
   googleClientId,
@@ -23,151 +25,152 @@ import {
 import '../../../public/styles/login/login.scss';
 
 class WelcomeBack extends React.Component {
-  constructor() {
-    super();
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.state = {
-      show: false,
-      message: null,
-    };
-  }
+	constructor() {
+		super();
+		this.handleShow = this.handleShow.bind(this);
+		this.handleClose = this.handleClose.bind(this);
+		this.state = {
+		show: false,
+		message: null,
+		};
+	}
 
-  handleClose() {
-    this.setState({ show: false, message: null });
-  }
+	handleClose() {
+		this.setState({ show: false, message: null });
+	}
 
-  handleShow() {
-    this.setState({ show: true, message: null });
-  }
+	handleShow() {
+		this.setState({ show: true, message: null });
+	}
 
-  // Social Login Handlers
-  responseGoogle = response => {
-    const profileData = response.profileObj;
-    if (!profileData.email) {
-      alert('SocialLogin - Email Id missing');
-      return;
-    }
+	// Social Login Handlers
+	responseGoogle = response => {
+		const profileData = response.profileObj;
+		if (!profileData.email) {
+		alert('SocialLogin - Email Id missing');
+		return;
+		}
 
-    this.setState({
-      firstName: profileData.givenName,
-      lastName: profileData.familyName,
-      authorizationProvider: 'google',
-      userId: response.googleId,
-      socialToken: response.accessToken,
-      emialId: profileData.email,
-    });
+		this.setState({
+		firstName: profileData.givenName,
+		lastName: profileData.familyName,
+		authorizationProvider: 'google',
+		userId: response.googleId,
+		socialToken: response.accessToken,
+		emialId: profileData.email,
+		});
 
-    onGoogleResponse(this.state, itemData => {
-      console.log('GoogleCallback', itemData);
-    });
-  };
+		onGoogleResponse(this.state, itemData => {
+		console.log('GoogleCallback', itemData);
+		});
+	};
 
-  responseFacebook = response => {
-    if (!response.email) {
-      alert('SocialLogin - Email Id missing');
-      return;
-    }
+	responseFacebook = response => {
+		if (!response.email) {
+		alert('SocialLogin - Email Id missing');
+		return;
+		}
 
-    // Remove below condition to get auto Facebook login.
-    if (this.state.isFacebookClicked) {
-      const firstName = response.name.substr(0, response.name.indexOf(' '));
-      const lastName = response.name.substr(response.name.indexOf(' ') + 1);
-      this.setState({
-        firstName,
-        lastName,
-        authorizationProvider: 'facebook',
-        userId: response.userID,
-        socialToken: response.accessToken,
-        emialId: response.email,
-      });
+		// Remove below condition to get auto Facebook login.
+		if (this.state.isFacebookClicked) {
+		const firstName = response.name.substr(0, response.name.indexOf(' '));
+		const lastName = response.name.substr(response.name.indexOf(' ') + 1);
+		this.setState({
+			firstName,
+			lastName,
+			authorizationProvider: 'facebook',
+			userId: response.userID,
+			socialToken: response.accessToken,
+			emialId: response.email,
+		});
 
-      onFacebookResponse(this.state, itemData => {
-        console.log('FacebookCallback', itemData);
-      });
-    }
-  };
+		onFacebookResponse(this.state, itemData => {
+			console.log('FacebookCallback', itemData);
+		});
+		}
+	};
 
-  facebookOnClick() {
-    this.setState({ isFacebookClicked: true });
-  }
+	facebookOnClick() {
+		this.setState({ isFacebookClicked: true });
+	}
 
-  /* Handle User Login API */
-  handleUserLoginApi(data) {
-    this.setState({ message: null });
-    axios
-      .post(userLoginAPI, data, {
-        headers: { store_id: storeId, access_token: accessToken },
-      })
-      .then(response => {
-        document.cookie = 'isLoggedIn=true';
-        document.cookie = `${accessTokenCookie}=${
-          response.data.data.access_token
-        }`;
-        alert('Successfully Logged In');
-        this.handleClose();
-      })
-      .catch(error => {
-        const errorData = error.response.data;
-        const errorMessage = errorData.error.error_message;
-        this.setState({
-          message: `Error - ${errorMessage}`,
-        });
-      });
-  }
+	/* Handle User Login API */
+	handleUserLoginApi(data) {
+		this.setState({ message: null });
+		axios
+		.post(userLoginAPI, data, {
+			headers: { store_id: storeId, access_token: accessToken },
+		})
+		.then(response => {
+			document.cookie = 'isLoggedIn=true';
+			document.cookie = `${accessTokenCookie}=${
+			response.data.data.access_token
+			}`;
+			alert('Successfully Logged In');
+			this.handleClose();
+		})
+		.catch(error => {
+			const errorData = error.response.data;
+			const errorMessage = errorData.error.error_message;
+			this.setState({
+			message: `Error - ${errorMessage}`,
+			});
+		});
+	}
 
-  render() {
-    let message = null;
-    if (this.state.message) {
-      message = <p>{this.state.message}</p>;
-    }
-    return (
-      <div>
-        <a className="dropDown" onClick={this.handleShow}>
-          Login/Register
-        </a>
-        <Modal
-          className="welcomeBack"
-          size="lg"
-          animation={false}
-          show={this.state.show}
-          onHide={this.handleClose}
-        >
-          {message}
-          <Modal.Header closeButton>
-            <Modal.Title>Welcome Back</Modal.Title>
-          </Modal.Header>
-          <div className="socialLogin">
-            <GoogleLogin
-              className="btn-white appLogin"
-              clientId={googleClientId}
-              buttonText="Sign in with Google"
-              cssClass="btn-white"
-              onSuccess={this.responseGoogle}
-              onFailure={this.responseGoogle}
-            />
-            <FacebookLogin
-              appId={facebookAppId}
-              autoLoad
-              fields="name,email,picture"
-              cssClass="appLogin"
-              buttonText="Sign in with Facebook"
-              onClick={this.facebookOnClick}
-              callback={this.responseFacebook}
-            />
-          </div>
-          <p className="divider">or</p>
-          <WelcomeBackForm
-            className="loginForm"
-            handleUserData={this.handleUserLoginApi.bind(this)}
-          />
-          <p className="registerHere">
-            New to Interio? <span>Register</span>
-          </p>
-        </Modal>
-      </div>
-    );
-  }
+	render() {
+		let message = null;
+		if (this.state.message) {
+		message = <p>{this.state.message}</p>;
+		}
+		return (
+			<div>
+				<a className="dropDown" onClick={this.handleShow}>
+				Login/Register
+				</a>
+				<Modal
+				className="welcomeBack"
+				size="lg"
+				animation={false}
+				show={this.state.show}
+				onHide={this.handleClose}
+				>
+					{message}
+					<Modal.Header closeButton>
+						<div className='smallLogo'><WhiteLogo /></div>
+						<Modal.Title>Welcome Back</Modal.Title>
+					</Modal.Header>
+					<div className="socialLogin">
+						<GoogleLogin
+						className="btn-white appLogin"
+						clientId={googleClientId}
+						buttonText="Sign in with Google"
+						cssClass="btn-white"
+						onSuccess={this.responseGoogle}
+						onFailure={this.responseGoogle}
+						/>
+						<FacebookLogin
+						appId={facebookAppId}
+						autoLoad
+						fields="name,email,picture"
+						cssClass="appLogin"
+						buttonText="Sign in with Facebook"
+						onClick={this.facebookOnClick}
+						callback={this.responseFacebook}
+						/>
+					</div>
+					<p className="divider">or</p>
+					<WelcomeBackForm
+						className="loginForm"
+						handleUserData={this.handleUserLoginApi.bind(this)}
+					/>
+					<p className="registerHere">
+						New to Interio? <JoinUs />
+					</p>
+				</Modal>
+			</div>
+		);
+	}
 }
 
 export default WelcomeBack;
