d3ab65a0de900971935302ec58e88c36bcdc6946
Dimple Jaiswal <djaiswal@salmonggn.com>
2019-05-30 18:40:15 +0530

    Add to campare and PLP changes are updated

--- app/src/components/PlpComponent/compItem.js
+++ app/src/components/PlpComponent/compItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ItemImage from '../GlobalComponents/productItem/image';
 import Price from '../GlobalComponents/productItem/price';
 import '../../../public/styles/compWidget.scss';
-
+import {newMachineUrl, store, catalog} from '../../../public/constants/constants';
 import close from '../../../public/images/close.svg';
 
 function CompItem(props) {
@@ -14,7 +14,7 @@ function CompItem(props) {
       <div className="imgBox">
         <img
           className="imgfullwidth"
-          src={`https://192.168.0.36:8443${props.product.thumbnail}`}
+          src={`${newMachineUrl}/${store}/${catalog}/${props.product.thumbnail}`}
         />
       </div>
 
