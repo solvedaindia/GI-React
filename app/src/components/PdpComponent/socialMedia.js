cf9ec9df3eee366d79c72ed5169eb12f9af83d3b
Manish Kumar Sangwan <mksangwan@salmonggn.com>
2019-05-30 17:57:42 +0530

    updated social-icons

--- app/src/components/PdpComponent/socialMedia.js
+++ app/src/components/PdpComponent/socialMedia.js
@@ -26,38 +26,46 @@ class SocialMedia extends React.Component {
 
 		return (
 			<>			<div className='social-icon-share'>
+							<ul>
+								<li>						
 								<FacebookShareButton
 									url={shareUrl}
 									quote={title}
 								>
 									<FacebookIcon
-									size={32}
+									size={26}
 									round />
-								</FacebookShareButton>Facebook
-                                <WhatsappShareButton
+								</FacebookShareButton><span className='social-icon-text'> Facebook</span></li>
+                                <li>
+								<WhatsappShareButton
 									url={shareUrl}
 									title={title}
 									separator=":: "
 									className="Demo__some-network__share-button">
-									<WhatsappIcon size={32} round />
-								</WhatsappShareButton>Whatsapp
+									<WhatsappIcon size={26} round />
+								</WhatsappShareButton><span className='social-icon-text'>Whatsapp</span>
+								</li>
+								<li>
                                 <TwitterShareButton
                                     url={shareUrl}
                                     title={title}
                                     className="Demo__some-network__share-button">
                                     <TwitterIcon
-                                    size={32}
+                                    size={26}
                                     round />
-                                </TwitterShareButton>Twitter
-
+                                </TwitterShareButton><span className='social-icon-text'>Twitter</span>
+								</li>
+								<li>
 								<EmailShareButton
 									url={shareUrl}
 									title={title}
 									className="Demo__some-network__share-button">
 									<EmailIcon
-									size={32}
+									size={26}
 									round />
-								</EmailShareButton>Mail
+								</EmailShareButton><span className='social-icon-text'>Mail</span>
+								</li>
+								</ul>
                                 </div>
 
 
