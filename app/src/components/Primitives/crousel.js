42c4667f697a32bc01e1327ef6d387565005daa5
mdsahir1990 <mdsahir@solveda.com>
2019-06-13 15:38:23 +0530

    static implementation of inspiration page

---
+++ app/src/components/Primitives/crousel.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+
+export default class Inscrousel extends Component {
+  constructor(props) {
+    super(props);
+    const img1 = <img className="Oval-Copy-6" src={require('../../../public/images/cr1.jpg')} alt="rectangle"/>
+    const img2 = <img className="Oval-Copy-6" src={require('../../../public/images/cr2.jpg')} alt="rectangle"/>
+    const img3 = <img className="Oval-Copy-6" src={require('../../../public/images/cr3.jpg')} alt="rectangle"/>
+    const img4 = <img className="Oval-Copy-6" src={require('../../../public/images/cr4.jpg')} alt="rectangle"/>
+    const img5 = <img className="Oval-Copy-6" src={require('../../../public/images/cr6.jpg')} alt="rectangle"/>
+    const img6 = <img className="Oval-Copy-6" src={require('../../../public/images/cr0.jpg')} alt="rectangle"/>
+
+    this.state = {
+      slides: [img1, img2, img3, img4, img5, img6]
+    };
+    this.click = this.click.bind(this);
+  }
+  click() {
+    const { slides } = this.state;
+    this.setState({
+      slides:
+        slides.length === 6 ? [img1, img2, img3, img4, img5, img6, "", "", ""] : [img1, img2, img3, img4, img5, img6]
+    });
+  }
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 3
+    };
+    return (
+      <div>
+        <h2 className="Browse-Lookbook-by-T">Browse Lookbook by Theme</h2>
+        
+        <Slider {...settings}>
+          {this.state.slides.map(function(slide) {
+            return (
+              <div key={slide}>
+                <h3>{slide}</h3>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    );
+  }
+}
--- app/src/containers/InspirationCont/index.js
+++ app/src/containers/InspirationCont/index.js
@@ -1,8 +1,3 @@
-/**
- *
- * CLPContainer
- *
- */
 
 import React from 'react';
 import FullBanner from '../../components/Primitives/slider';
@@ -10,6 +5,9 @@ import LivingTheme from '../../components/ClpComponent/subComponents/livingInspi
 import SubCategory from '../../components/GlobalComponents/subCategory';
 import BestSeller from '../../components/BestSelling/bestSelling';
 import ReadMore from '../../components/GlobalComponents/readMore';
+import  '../../../public/styles/Inspiration/inspiration.scss'
+import InsCrousel from '../../components/Primitives/crousel'
+
 
 export class Inspiration extends React.Component {
   constructor(props) {
@@ -24,15 +22,108 @@ export class Inspiration extends React.Component {
 
   render() {
     return (
-      <section className="clpBase">
-        <div className="slider">
-          	<FullBanner />
-			<SubCategory />
-			<LivingTheme />
-			<BestSeller />
-			<ReadMore />
-        </div>
-      </section>
+      <div className="container">
+       <div> <img 
+                      className="Rectangle"
+                      src={require('../../../public/images/product-slider.jpg')}
+                      alt="rectangle"
+                    />
+                   
+                    </div>
+                    <InsCrousel />
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-md-6">
+                        <img vspace="50" 
+                      className="RectImg"
+                      src={require('../../../public/images/sm1.jpg')}
+                      alt="rectangle"
+                    />
+                     <h4 className="Summer-Spark">Summer Spark</h4>
+                    <p1 className="Paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam urna, ullamcorper sit amet arcu sed, viverra malesuada mi. Nam rutrum vulputate lectus vel tincidunt. </p1>
+                        </div>
+                        <div className="col-md-6">
+                        <img vspace="50"
+                      className="RectImg"
+                      src={require('../../../public/images/sm2.jpg')}
+                      alt="rectangle"
+                    />
+                      <h4 className="Summer-Spark">Summer Spark</h4>
+                    <p1 className="Paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam urna, ullamcorper sit amet arcu sed, viverra malesuada mi. Nam rutrum vulputate lectus vel tincidunt. </p1>
+                        </div>
+
+                      </div>
+                      <div className="row">
+                      <div className="col-md-6">
+                      <img vspace="50"
+                      className="RectImg"
+                      src={require('../../../public/images/sm3a.jpg')}
+                      alt="rectangle"
+                    />
+                     <h4 className="Summer-Spark">Summer Spark</h4>
+                    <p1 className="Paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam urna, ullamcorper sit amet arcu sed, viverra malesuada mi. Nam rutrum vulputate lectus vel tincidunt. </p1>
+                      </div>
+                      <div className="col-md-6">
+                      <img vspace="50"
+                      className="RectImg"
+                      src={require('../../../public/images/sm4.jpg')}
+                      alt="rectangle"
+                    />
+                     <h4 className="Summer-Spark">Summer Spark</h4>
+                    <p1 className="Paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam urna, ullamcorper sit amet arcu sed, viverra malesuada mi. Nam rutrum vulputate lectus vel tincidunt. </p1>
+                      </div>
+
+
+                      </div>
+                      <div className="row">
+                        <div className="col-md-6">
+                        <img vspace="50"
+                      className="RectImg"
+                      src={require('../../../public/images/sm5a.jpg')}
+                      alt="rectangle"
+                    />
+                     <h4 className="Summer-Spark">Summer Spark</h4>
+                    <p1 className="Paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam urna, ullamcorper sit amet arcu sed, viverra malesuada mi. Nam rutrum vulputate lectus vel tincidunt. </p1>
+                        </div>
+                      <div className="col-md-6">
+                      <img vspace="50"
+                      className="RectImg"
+                      src={require('../../../public/images/sm6.jpg')}
+                      alt="rectangle"
+                    />
+                     <h4 className="Summer-Spark">Summer Spark</h4>
+                    <p1 className="Paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam urna, ullamcorper sit amet arcu sed, viverra malesuada mi. Nam rutrum vulputate lectus vel tincidunt. </p1>
+                      </div>
+                      </div>
+                      
+
+                      <div className="row">
+                      <div className="col-md-6">
+                      <img vspace="50"
+                      className="RectImg"
+                      src={require('../../../public/images/sm7.jpg')}
+                      alt="rectangle"
+                    />
+                     <h4 className="Summer-Spark">Summer Spark</h4>
+                    <p1 className="Paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam urna, ullamcorper sit amet arcu sed, viverra malesuada mi. Nam rutrum vulputate lectus vel tincidunt. </p1>
+                      </div>
+                      <div className="col-md-6">
+                      <img vspace="50"
+                      className="RectImg"
+                      src={require('../../../public/images/sm8.jpg')}
+                      alt="rectangle"
+                    />
+                     <h4 className="Summer-Spark">Summer Spark</h4>
+                    <p1 className="Paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam urna, ullamcorper sit amet arcu sed, viverra malesuada mi. Nam rutrum vulputate lectus vel tincidunt. </p1>
+                      </div>
+
+                      </div>
+
+
+
+                      </div>
+        
+      </div>
     );
   }
 }
