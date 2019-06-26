52b6e3322753241dd23d39368fdd28fba1f8f3c6
Mritunjay Kumar <mkumar@salmonggn.com>
2019-06-26 13:50:06 +0530

    AutoSugges UI and cross function added

--- app/src/components/Search/search.js
+++ app/src/components/Search/search.js
@@ -14,10 +14,16 @@ class SearchBar extends React.Component {
     }
     this.handleClick = this.handleChange.bind(this);
     this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    this.clearFields= this.clearFields.bind(this);
+    this.showButton= this.showButton.bind(this);
   }
 
   handleChange = event => {
     const searchText = event.target.value;
+    if (event.target.value === '') {            
+      var crossbtn=document.getElementById('clearField');
+      crossbtn.style.display='none';
+  }
     this.setState({
       searchData: [],
     });
@@ -49,39 +55,42 @@ class SearchBar extends React.Component {
     }
   }
 
+  clearFields(e){         
+    var inputVal= document.getElementById("searchInput").value='';     
+    var crossbtn = document.getElementById('clearField');
+    crossbtn.style.display='none' 
+}
+showButton(e){ 
+  var crossbtn = document.getElementById('clearField');
+  crossbtn.style.display='block'
+}
+  
   render() {
     const searchData = this.state.searchData;
     return (
-      <div className='searchBar'>
-        <SearchLogo />
-        <input className='searchInput' id='searchInput' onChange={this.handleChange} onClick={this.handleChange} type='text' autoComplete='off' placeholder='search for products' />
-        {searchData.length > 0 &&
-          <div id="autoSuggestDiv" ref={node => { this.node = node; }}>
-            <ul className='auto-search'>
-              <li className="list">
-                <a className="link" href="#">
-                  Suggestions
-                </a>
-                {searchData.map((item, index) => {
-                  const searchItem = document.getElementById("searchInput").value;
-                  if (index < 6) {
-                    return (
-                      <li className='list' key={index}><a className='link' href='#'><strong>{item.term.substr(0, searchItem.length)}</strong>{item.term.substr(searchItem.length)}</a></li>
-                    );
-                  }
-                })
-                }
-              </li>
-            </ul>
-          </div>
-        }
-      </div>
+        <div className='searchBar'>
+            <SearchLogo />                
+            <input className='searchInput' id='searchInput' onKeyPress={this.showButton} onChange={this.handleChange} onClick={this.handleChange} type='text' autoComplete='off' placeholder='search for products' />
+            <a className='clearField' id='clearField' role='button' onClick={this.clearFields}>X</a>
+            { searchData.length > 0 && 
+                <div id='autoSuggestDiv' ref={node => { this.node = node; }}>
+                    <ul className='auto-search'>
+                    <li className='list'><a className='link' href='#'>Suggestions</a></li>
+                        { searchData.map((item, index) => {    
+                            const searchItem = document.getElementById("searchInput").value;
+                            if (index < 6) {
+                                return(
+                                    <li className='list' key={index}><a className='link' href='#'><strong>{ item.term.substr(0, searchItem.length) }</strong>{item.term.substr(searchItem.length)}</a></li>
+                                );
+                            }
+                            })
+                        }
+                    </ul>
+                </div>
+            }
+        </div>
     );
-
-  }
-
-
-
+}
 
 }
 
