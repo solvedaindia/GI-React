e4cc3d68129fc929714f027ee75933e3aa14b4a2
Dimple Jaiswal <djaiswal@salmonggn.com>
2019-07-01 11:03:14 +0530

    Cart changes

--- app/src/components/BestSelling/bestSelling.js
+++ app/src/components/BestSelling/bestSelling.js
@@ -5,7 +5,7 @@ import {
   bestSellerAPI,
   imagePrefix,
 } from '../../../public/constants/constants';
-
+import {is} from '../../utils/utilityManager';
 import '../../../public/styles/bestSeller/bestSeller.scss';
 import '../../../public/styles/slickCustom.scss';
 
@@ -21,10 +21,11 @@ class BestSeller extends React.Component {
     apiManager
       .get(bestSellerAPI)
       .then(response => {
+		const { data } = response || {};
+		const bsData = data && data.data;
         this.setState({
-          bestSellerData: response.data.data.productList,
-          title: response.data.data.productList,
-          isLoading: false,
+			bestSellerData: is(bsData, 'Object') && bsData,
+			isLoading: false,
         });
         console.log('BestSellerData#########', response.data.data);
       })
@@ -53,10 +54,9 @@ class BestSeller extends React.Component {
     };
     return (
       <div className="bestSeller">
-        <h1 className="title">Best Selling Items</h1>
+        <h1 className="title">{bestSellerData.title}</h1>
         <Slider {...settings}>
-          {!!bestSellerData &&
-            bestSellerData.map((sellerItemData, index) => (
+		{!!bestSellerData && bestSellerData.productList.map((sellerItemData, index) => (
               <figure key={index} className="bsSlides">
                 <a href={sellerItemData.onClickUrl}>
                   <img
