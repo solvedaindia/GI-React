bc794dc9064d3560a031da33361f8fbfd8f697bd
Dimple Jaiswal <djaiswal@salmonggn.com>
2019-07-18 21:12:28 +0530

    Track order - homepage UI

--- app/src/components/TrackOrder/trackOrder.js
+++ app/src/components/TrackOrder/trackOrder.js
@@ -1,34 +1,53 @@
 import React from 'react';
+import Slider from 'react-slick';
 import apiManager from '../../utils/apiManager';
 import {
     trackOrderMiniAPI,
+    userDetailAPI
 } from '../../../public/constants/constants';
 import { getCookie } from '../../utils/utilityManager';
+import '../../../public/styles/trackMiniOrder/trackMiniOrder.scss';
 
 class TrackOrder extends React.Component {
     constructor(props){
         super(props);
         this.state = {
+            orderData: null,
             isLoading: true,
-            errors: null
+            errors: null,
+            userInfo: null
         } 
     }
-
+    getUserDetails() {
+        apiManager
+        .get(userDetailAPI)
+        .then(response => {
+            this.setState({
+                userInfo : response.data.data,
+                isLoading: false
+            });
+        })
+        .catch(error => {
+            this.setState({
+                error,
+                isLoading: false
+            });
+        })
+    }
     getTracOrderDetails() {
         apiManager
         .get(trackOrderMiniAPI)
         .then(response => {
-            const {data} = response || {};
             this.setState({
-                recoData: data && data.data,
-                isLoading: false,
+                orderData: response.data.data,
+                isLoading: false
             });
             console.log(' Mini Cart Data', data.data);
         })
         .catch(error => {
             this.setState({
             error,
-            isLoading: false,
+            isLoading: false
             });
             console.log('ERROR');
         });
@@ -36,10 +55,47 @@ class TrackOrder extends React.Component {
     componentDidMount() {
         if (getCookie('isLoggedIn') === 'true') {
             this.getTracOrderDetails();
+            this.getUserDetails();
         }
     }
     render() {
-        return <p>Hi there!</p>
+        const { orderData, userInfo } = this.state;
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          };
+        return (
+            <section className='trackMiniOrder'>
+                <div className='userDetails'>
+                    <p className='userName'>Hello {!!userInfo && userInfo.name}</p>
+                    <p>Track Your Order</p>
+                </div>
+                <div className='orderDetails'>
+                <Slider {...settings}>
+                    {!!orderData && orderData.ongoingOrders.map((orderDetails) => {
+                        return(
+                            // <p>
+                            //     {orderDetails.orderID}
+                            // </p>
+                            orderDetails.orderItems.map((subOrderDetails, index) => {
+                                return(
+                                    <div className='orderSummary'>
+                                        <figure className='prodImg'>
+                                            <img src='https://192.168.0.57/imagestore/images/godrejInterio/pdp/sampleImages/56101502SD00473/56101502SD00473_546x307_01.png' alt={index}/>
+                                        </figure>
+
+                                    </div>
+                                )
+                            })
+                        )
+                    })}
+                    </Slider>
+                </div>
+            </section>
+        )
     }
 }
 
