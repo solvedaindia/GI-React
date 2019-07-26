1fdac3ab7300a25e9c5176b4feca2df49de49c5f
Deepak <dsahu@solveda.com>
2019-07-26 17:10:25 +0530

    Fixed JIRA's: 1102, 1061, RWD Footer  Accordian

--- app/src/containers/FooterContainer/FooterRWD/footerLinkAccordian.js
+++ app/src/containers/FooterContainer/FooterRWD/footerLinkAccordian.js
@@ -3,37 +3,75 @@ import { Col } from 'react-bootstrap';
 import Accordion from './Accordion';
 // import '../../../../public/styles/footerContainer/accordian.scss'
 
-const FooterLinkAccordian = props => {
-  const footerLinksData = props.name.map((val, index) => (
-    <Col key={index} md={3} sm={3}>
-      <Accordion>
-        {val.map((item, childindex) => (
-          <div className="accor footerlink_wrpper">
-            <h5 className="head heading">
-              {item.text}
-              <span className="plusIcon">
-                <img
-                  src={require('../../../../public/images/plusIconWhite.svg')}
-                />
-              </span>
-            </h5>
-            <div className="body">
-              <ul>
-                {item.children.map((litem, i) => (
-                  <li className="list" key={i}>
-                    <a className="link" href={litem.action}>
-                      {litem.text}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
+// const FooterLinkAccordian = props => {
+class FooterLinkAccordian extends React.Component {
+
+
+  // const footerLinksData = props.name.map((val, index) => (
+  //   <Col key={index} md={3} sm={3}>
+  //     <Accordion>
+  //       {val.map((item, childindex) => (
+  //         <div className="accor footerlink_wrpper">
+  //           <h5 className="head heading">
+  //             {item.text}
+  //             <span className="plusIcon" onClick={this._handleClick}>
+  //               <img src={require('../../../../public/images/plusIconWhite.svg')} />
+  //             </span>
+  //           </h5>
+  //           <div className="body">
+  //             <ul>
+  //               {item.children.map((litem, i) => (
+  //                 <li className="list" key={i}>
+  //                   <a className="link" href={litem.action}>
+  //                     {litem.text}
+  //                   </a>
+  //                 </li>
+  //               ))}
+  //             </ul>
+  //           </div>
+  //         </div>
+  //       ))}
+  //     </Accordion>
+  //   </Col>
+  // ));
+  // return footerLinksData;
+
+  render() {
+
+    return (
+      <>
+        {this.props.name.map((val, index) => (
+          <Col key={index} md={3} sm={3}>
+            <Accordion>
+              {val.map((item, childindex) => (
+                <div className="footerlink_wrpper accor" id="footerlink_wrpper">
+                  <h5 className="head heading">
+                    {item.text}
+                    <span className="plusIcon" onClick={this._handleClick}>
+                      <img src={require('../../../../public/images/plusIconWhite.svg')} />
+                    </span>
+                  </h5>
+                  <div className="body">
+                    <ul>
+                      {item.children.map((litem, i) => (
+                        <li className="list" key={i}>
+                          <a className="link" href={litem.action}>
+                            {litem.text}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ))}
+            </Accordion>
+          </Col>
         ))}
-      </Accordion>
-    </Col>
-  ));
-  return footerLinksData;
-};
+      </>
+    );
+  }
+
+
+}
 
 export default FooterLinkAccordian;
