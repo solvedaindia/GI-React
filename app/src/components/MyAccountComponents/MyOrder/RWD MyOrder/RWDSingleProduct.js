2c2895d67b6a67c1e411c485b5b3edf85ff1f00e
Deepak <dsahu@solveda.com>
2019-08-02 16:21:27 +0530

    RWDSingleProduct Created

---
+++ app/src/components/MyAccountComponents/MyOrder/RWD MyOrder/RWDSingleProduct.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import apiManager from '../../../../utils/apiManager';
+import { changePasswordAPI, imagePrefix } from '../../../../../public/constants/constants';
+
+class RWDSingleProduct extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+
+  render() {
+    return (
+      <>
+       <h2>Single Item</h2>
+      </>
+    );
+  }
+}
+
+export default RWDSingleProduct;
