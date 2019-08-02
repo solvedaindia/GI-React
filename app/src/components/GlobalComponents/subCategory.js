49c7dc5706635fd73ef1bbbfa67b232b6e4c997c
Dimple Jaiswal <djaiswal@salmonggn.com>
2019-08-02 11:55:10 +0530

    ESPOT changes

--- app/src/components/GlobalComponents/subCategory.js
+++ app/src/components/GlobalComponents/subCategory.js
@@ -47,7 +47,7 @@ export class SubCategory extends React.Component {
     const { subCatData } = this.state;
     const settings = {
       dots: false,
-      infinite: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 4,
       slidesToScroll: 1,
