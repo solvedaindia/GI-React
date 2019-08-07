23692b4daab391570edd17d90450a4f0ef199ce6
Mritunjay Kumar <mkumar@salmonggn.com>
2019-08-07 14:33:23 +0530

    Help support Ui style

--- app/src/containers/serviceSupportContainer/index.js
+++ app/src/containers/serviceSupportContainer/index.js
@@ -19,112 +19,85 @@ export default class HelpSupport extends React.Component {
     return (
       <div className='serviceSupport'>
         <div className='container'>
-        <h1 className="Header-Copy-h1">Service & Support</h1>
-        <h3 className="Header-Copy-h3">How can we help you?</h3>
-       
-        <div className="container">
-        <div className="row">
-          <div className="container">
-          <div className="col-md-2">
-            <h3 className="faqhead">Frequently Asked Questions (FAQ)</h3>
-            <ul>
-              <li>
-                <button className="uldiv">My Orders</button>
-              </li>
-              <li>
-                <button className="uldivlight">Before I Buy</button>
-
-              </li>
-              <li>
-                <button className="uldivlight">Payments</button>
-
-              </li>
-              <li>
-                <button className="uldivlight">Warranty and Post Delivery Services</button>
-
-              </li>
-              <li>
-                <button className="uldivlight">My Order Queries</button>
-              </li>
-              <li>
-                <button className="uldivlight">Cancellations and Returns</button></li>
-              <li>
-                <button className="uldivlight">Gift Cards</button>
-
-              </li>
-              <li>
-                <button className="uldivlight">Request Service</button>
-
-              </li>
+        <h1 className="headingServicesupport">Service & Support</h1>
+        <h3 className="subHeading">How can we help you?</h3>      
+        
+        <div className="row innerContent">
+          <div className="col-md-3 faqArea">
+            <h2 className="faqhead">Frequently Asked Questions (FAQ)</h2>
+            <ul className='faqList'>
+              <li className='faqItem active'>My Orders</li>
+              <li className='faqItem'>Before I Buy</li>
+              <li className='faqItem'>Payments</li>
+              <li className='faqItem'>Warranty and Post Delivery Services</li>
+              <li className='faqItem'>My Order Queries</li>
+              <li className='faqItem'>Cancellations and Returns</li>
+              <li className='faqItem'>Gift Cards</li>
+              <li className='faqItem'>Request Service</li>
             </ul>
           </div>
-          <div className="col-md-10">
-            
-          <h3 className="MyOd">My Orders</h3>
-
+          <div className="col-md-9 myOrder">            
+          <h2 className="myOrders">My Orders</h2>
             <FaqAnswers />
             </div>
             
             
             <div className='clearfix'></div>
-           
         </div>
-        </div>
-        </div>
+        
         <div className='clearfix'></div>
+       
+
+        </div>
+        <div className='customerCare'>
         <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+            <h1 className="headingCustomercare">Contact Customer Care</h1>
+            </div>
 
-<div className="row">
-  <div className="col-md-12">
-  <h1 className="Header-Copy-4">Contact Customer Care</h1>
-  </div>
+            <div className="col-md-6 borderRight">
+              <h2 className="enquiry">Enquiries</h2>
+              <div className="row">
+                <div className="col-sm-6 text-center">
+                  <img className="icon" src={require('../../../public/images/emailus.png')} alt="Rectangle" />
+                  <h4 className="contactus">Email</h4>
+                  <h4 className="emailsmall">talktous@godrej.com</h4>
 
-  <div className="col-md-6">
-    <h4 className="enquiry">Enquiries</h4>
-    <div className="row">
-      <div className="col-sm-6 text-center">
-        <img className="emailus" src={require('../../../public/images/emailus.png')} alt="Rectangle" />
-        <h4 className="contactus">Email</h4>
-        <h4 className="emailsmall">talktous@godrej.com</h4>
-
-      </div>
-      <div className="col-sm-6 text-center">
-      <img className="emailus" src={require('../../../public/images/1034131.svg')} alt="Rectangle" />
-      <h4 className="contactus">Contact-Us</h4>
-      <h4 className="emailsmall">+91 8654123547+91 6325415873</h4>
-      </div>
-    </div>
-  </div>
-  <div className="col-md-6">
-    <h4 className="enquiry">After Sales Services</h4>
-    <div className="row">
-      <div className="col-sm-6 text-center">
-        <img className="emailus" src={require('../../../public/images/emailus.png')} alt="Rectangle" />
-        <h4 className="contactus">Email</h4>
-        <h4 className="emailsmall">talktous@godrej.com</h4>
-      </div>
-      <div className="col-sm-6 text-center">
-      <img className="emailus" src={require('../../../public/images/1034131.svg')} alt="Rectangle" />
-      <h4 className="contactus">Contact-Us</h4>
-      <h4 className="emailsmall">+91 8654123547+91 6325415873</h4>
-      </div>
-    </div>
-  
-  </div>
-</div>
-
-</div>
+                </div>
+                <div className="col-sm-6 text-center">
+                <img className="icon" src={require('../../../public/images/1034131.svg')} alt="Rectangle" />
+                <h4 className="contactus">Contact-Us</h4>
+                <h4 className="emailsmall">+91 8654123547 <br/>+91 6325415873</h4>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <h2 className="enquiry">After Sales Services</h2>
+              <div className="row">
+                <div className="col-sm-6 text-center">
+                  <img className="icon" src={require('../../../public/images/emailus.png')} alt="Rectangle" />
+                  <h4 className="contactus">Email</h4>
+                  <h4 className="emailsmall">talktous@godrej.com</h4>
+                </div>
+                <div className="col-sm-6 text-center">
+                <img className="icon" src={require('../../../public/images/1034131.svg')} alt="Rectangle" />
+                <h4 className="contactus">Contact-Us</h4>
+                <h4 className="emailsmall">+91 8654123547<br/>+91 6325415873</h4>
+                </div>
+              </div>
+            
+            </div>
+          </div>
+       </div>
         </div>
-        <button className="chat">
-      <div className="row">
-        <div className="col-md-6">
-          <h4 className="chattext">Live Chat </h4>
-        </div>
-        <div className="col-md-6">
-          <img className="chatIcon" src={require('../../../public/images/chat.png')} alt="Rectangle" />
-        </div>
-      </div>
-    </button>
+       
+        
+        <button className="Livechat">
+        <span className="text">Live Chat </span>
+        <span className='iconChat'><img className="chatIcon" src={require('../../../public/images/chat.png')} alt="Rectangle" /></span>
+            
+       </button>
       </div>
     );
   }
