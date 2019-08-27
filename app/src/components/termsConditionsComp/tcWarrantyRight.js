734ceecaae9f7005126ff8cb5aeef3d89b9d104f
mdsahir1990 <mdsahir@solveda.com>
2019-08-27 13:43:11 +0530

    terms conditions splitted espots implemented

---
+++ app/src/components/termsConditionsComp/tcWarrantyRight.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import apiManager from '../../utils/apiManager';
+import {
+  espotAPI,
+  storeId,
+  accessToken,
+} from '../../../public/constants/constants';
+import '../../../public/styles/content.scss';
+
+class WarrantyRight extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      espotName: "GI_TERMS_AND_CONDITIONS_WARRANTIESANDLILIABILITES",
+      pageLayoutEspot: null,
+      isLoading: true,
+      error: null,
+    };
+  }
+
+  getEspotData() {
+    apiManager
+      .get(espotAPI + this.state.espotName)
+      .then(response => {
+        console.log('respo', response)
+        const {data} = response || {};
+        this.setState({
+          pageLayoutEspot: data && data.data,
+          isLoading: false,
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error,
+          isLoading: false,
+        });
+        console.log('Homepage Layout Espot Data ERROR');
+      });
+  }
+
+  componentDidMount() {
+    this.getEspotData();
+  }
+
+  render() {
+    const { pageLayoutEspot, index } = this.state;
+    if(!pageLayoutEspot) return null;
+    return (
+		!!pageLayoutEspot && (
+			<div>
+				<div dangerouslySetInnerHTML={{ __html: pageLayoutEspot.content }} />
+			</div>
+		)
+    );
+  }
+}
+
+export default WarrantyRight;
--- app/src/containers/termsAndConditions/index.js
+++ app/src/containers/termsAndConditions/index.js
@@ -1,7 +1,21 @@
 
 import React from 'react';
 import  '../../../public/styles/static-pages/termsconditions.scss';
-import Terms from '../../components/termsConditionsComp/termConditions';
+import GeneralProvision from '../../components/termsConditionsComp/tcGeneralPro';
+import Amendments from '../../components/termsConditionsComp/tcAmendments';
+import Communication from '../../components/termsConditionsComp/tcCommunication';
+import Coupans from '../../components/termsConditionsComp/tcCoupans';
+import Disclaimer from '../../components/termsConditionsComp/tcDisclaimer';
+import Materials from '../../components/termsConditionsComp/tcMaterial';
+import Payments from '../../components/termsConditionsComp/tcPayment';
+import PropertyRights from '../../components/termsConditionsComp/tcPropertyRights';
+import Selling from '../../components/termsConditionsComp/tcSelling';
+import USerAccount from '../../components/termsConditionsComp/tcUSerAccount';
+import Grievance from '../../components/termsConditionsComp/tcGrievance';
+import Membership from '../../components/termsConditionsComp/tcMembership';
+import WebsiteUSe from '../../components/termsConditionsComp/tcWebsiteUSe';
+import Violations from '../../components/termsConditionsComp/tcViolations';
+import WarrantyRight from '../../components/termsConditionsComp/tcWarrantyRight';
 
 
 export class TermsConditions extends React.Component {
@@ -15,9 +29,25 @@ export class TermsConditions extends React.Component {
 
   render() {
     return (    
-      <>     
-      <Terms/> 
-      </>
+      <div className='termsContainer'>
+        <div className='container'>
+          <Disclaimer />
+          <Membership />
+          <Communication />
+          <WebsiteUSe />
+          <Payments />
+          <Selling />
+          <Coupans />
+          <Amendments />
+          <USerAccount />
+          <Materials />
+          <PropertyRights />
+          <WarrantyRight />
+          <Violations />
+          <GeneralProvision />
+          <Grievance />
+        </div>
+      </div>
     )
   }
 }
