b1d1f447ab94faedcb1bd26213b8012eceee706a
mdsahir1990 <mdsahir@solveda.com>
2019-08-28 23:08:32 +0530

    lastest espots of TC

--- app/src/containers/termsAndConditions/index.js
+++ app/src/containers/termsAndConditions/index.js
@@ -18,35 +18,9 @@ export class TermsConditions extends React.Component {
     return (    
       <div className='termsContainer'>
         <div className='container'>
-       
-      
-           <ContentEspot espotName={ 'GI_Terms_and_Conditions_Disclaimer' } />
-          <ContentEspot espotName={ 'GI_Terms_and_Conditions_Membership' } />
-          <ContentEspot espotName={ 'GI_Terms_and_Conditions_WebsiteUse' } />
-
-          <ContentEspot espotName={ 'GI_Terms_and_Conditions_PlatformForCommunication' } />
-
-          <ContentEspot espotName={ 'GI_Terms_and_Conditions_PaymentFacility' } />
-
-          <ContentEspot espotName={ 'GI_Terms_and_Conditions_BreachAndSelling' } />
-
-          <ContentEspot espotName={ 'GI_Terms_and_Conditions_Coupons' } />
-
-         <ContentEspot espotName={ 'GI_Terms_and_Conditions_Amendments' } />
-
-            <ContentEspot espotName={ 'GI_Terms_and_Conditions_UserAccount' } />
-
-           <ContentEspot espotName={ 'GI_Terms_and_Conditions_MaterialsAndUsage' } />
-
-          <ContentEspot espotName={ 'GI_Terms_and_Conditions_PropertyRights' } />
-
-          <ContentEspot espotName={ 'GI_Terms_and_Conditions_WarrantiesAndLiliabilites' } />
-
-       <ContentEspot espotName={ 'GI_Terms_and_Conditions_ViolationToNewsletter' } />
-
-          <ContentEspot espotName={ 'GI_Terms_and_Conditions_GeneralProvisions' } />
-
-           <ContentEspot espotName={ 'GI_Terms_and_Conditions_GrievanceOfficer' } />
+           <ContentEspot espotName={ 'GI_TERMS_AND_CONDITIONS_DISCLAIMER' } />
+          <ContentEspot espotName={ 'GI_TERMS_AND_CONDITIONS_WARRANTIESANDLILIABILITES' } />
+          <ContentEspot espotName={ 'GI_TERMS_AND_CONDITIONS_USERACCOUNT' } />
 
         </div>
       </div>
