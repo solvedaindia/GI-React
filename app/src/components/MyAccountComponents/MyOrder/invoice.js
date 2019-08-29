5223601d72db6d0e1a9142034c1c67c3585dcb77
Dimple Jaiswal <djaiswal@salmonggn.com>
2019-08-29 18:44:29 +0530

    Invoice Layout creation

--- app/src/components/MyAccountComponents/MyOrder/invoice.js
+++ app/src/components/MyAccountComponents/MyOrder/invoice.js
@@ -11,7 +11,7 @@ class Invoice extends React.Component {
         this.state = {
             invoiceData: null
         }
-        this.download = this.download.bind(this);
+        // this.download = this.download.bind(this);
     }
     componentDidMount() {
         this.getInvoiveDetails();
@@ -24,9 +24,9 @@ class Invoice extends React.Component {
 				invoiceData: response.data.data,
 				isLoading: false,
             },
-            () => {
-                this.setup();
-            }
+            // () => {
+            //     this.setup();
+            // }
             );
 		})
 		.catch(error => {
@@ -37,39 +37,55 @@ class Invoice extends React.Component {
 			console.log('Invoive data error');
 		});
 	}
-    download=( )=>{
-        this.doc.save('invoice.pdf');
-    }
-    setup() {
-        const doc = new jsPDF();
-        const el = document.getElementById('content');
-        if (typeof(el)==='object' && el!==null) {
-            const width = 210
-            const elementHandlers = {
-                '#ignorePDF': (element,renderer)=>{
-                    return true
-                }
-            }
-            doc.fromHTML(el,1,1,{width,elementHandlers},()=>{
-                const pdf = doc.output('datauristring')
-                if (typeof(pdf)==='string' && pdf.length>0) {
-                this.setState({pdf})
-                }
-            })
+    // download=( )=>{
+    //     this.doc.save('invoice.pdf');
+    // }
+    // setup() {
+    //     const doc = new jsPDF();
+    //     const el = document.getElementById('content');
+    //     if (typeof(el)==='object' && el!==null) {
+    //         const width = 210
+    //         const elementHandlers = {
+    //             '#ignorePDF': (element,renderer)=>{
+    //                 return true
+    //             }
+    //         }
+    //         doc.fromHTML(el,1,1,{width,elementHandlers},()=>{
+    //             const pdf = doc.output('datauristring')
+    //             if (typeof(pdf)==='string' && pdf.length>0) {
+    //             this.setState({pdf})
+    //             }
+    //         })
             
-        }
-        this.doc = doc;
-    }
+    //     }
+    //     this.doc = doc;
+    // }
     invoiceDatailedData() {
         const { invoiceData } = this.state;
         console.log('!!!!! Invoice' , invoiceData);
+        // if(!invoiceData) return null;
         return (
             <div className="invoiceContainer" style={{width: '95%', margin: 'auto'}}>
+                <h3 style={{textAlign: 'center'}}>TAX INVOICE </h3>
                 {
-                    <div className="invoiceData">
-                        <h3 style={{textAlign: 'center'}}>TAX INVOICE</h3>
-                        <p>Invoice Body</p>
+                    !!invoiceData && <div className="invoiceData">
+                        <p>Cin No - {invoiceData.cinNO}</p>
+                        <p>Invoice Data - {invoiceData.invoiceDate}</p>
+                        <p> Invoice Time- {invoiceData.invoiceTime}</p>
+                        <p>Order Number - {invoiceData.orderNo}</p>
+                        <p>Company Name - {!!invoiceData.companyAddress && invoiceData.companyAddress.name}</p>
+                        <p>Company Address - {!!invoiceData.companyAddress && invoiceData.companyAddress.address}</p>
+                        <ul>
+                        {!!!!invoiceData.lineItemDetails && invoiceData.lineItemDetails.lineItemList.map((itemList, index) => {
+                            <li className='itemList' index={`${index}-item`}>
+                                <p>Item Name - {itemList.itemDesc}</p>
+                                <p>Item Amount - {itemList.itemTotalAmount}</p>
+                            </li>
+                        })}
+                        </ul>
+                        <p>Total Price - {!!invoiceData.lineItemDetails && invoiceData.lineItemDetails.totalAmount}</p>
                     </div>
+
                 }
             </div>
         )
@@ -80,7 +96,7 @@ class Invoice extends React.Component {
                 <div id="content">
                     {this.invoiceDatailedData()}
                 </div>
-                <p className='downloadBtn'><button className="btn btn-outline-primary" onClick={this.download}>Downlaod Invoice</button></p>
+                {/* <p className='downloadBtn'><button className="btn btn-outline-primary" onClick={this.download}>Downlaod Invoice</button></p> */}
             </div>
         );
     }
