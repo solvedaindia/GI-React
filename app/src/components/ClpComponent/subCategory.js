557859e00c338be9c866527b6202a81a3b21eb0b
dSahuSolveda <team@123455>
2019-09-06 11:57:24 +0530

    Code checked in for 1389 and comment removal

--- app/src/components/ClpComponent/subCategory.js
+++ app/src/components/ClpComponent/subCategory.js
@@ -100,8 +100,7 @@ export class SubCategory extends React.Component {
                     <Link to={routePath}>
                     <img className="subCatImg" src={`${imagePrefix}${subCatListData.thumbnail}`} // src={subCatListData.thumbnail} alt={subCatListData.categoryName}
                     />
-                    </Link>
-                    {/* <img src='https://192.168.0.36:8443/wcsstore/SolvedaCommerceCatalogAssetStore//images/catalog/apparel/women/wcl000_dresses/200x310/wcl000_0028_a_red.jpg' /> */}
+                    </Link>              
                   </a>
                   <figcaption className="catDetails">
                     <h2 className="catItem">{subCatListData.categoryName}</h2>
