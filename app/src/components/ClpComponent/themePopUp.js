ec2b79a1d43d1254a673ed415adb135acb828414
bpanday <bpandey@solveda.com>
2019-09-18 18:39:51 +0530

    GI-858 - JIra Issue

--- app/src/components/ClpComponent/themePopUp.js
+++ app/src/components/ClpComponent/themePopUp.js
@@ -27,7 +27,7 @@ class ThemeData extends React.Component {
         var currentLeftCords = "62";
         var currentBottomCords = "66";
         
-        if(isMobile()){
+        /*if(isMobile()){
             if(currentLeftCords < Number(y)){
                 this.setState({currentLeftCoords: "0px 0px 0px -211px"});
 
@@ -41,7 +41,33 @@ class ThemeData extends React.Component {
             }else{
                 this.setState({currentLeftCoords: "0px 0px 0px 0px"});
             }
-        }    
+        }  */
+        
+        if(isMobile()){
+            if(window.screen.width < 767){
+                if(62 < Number(y)){
+                    this.setState({currentLeftCoords: "0px 0px 0px -211px"});
+
+                if(currentBottomCords < Number(x)){
+                        this.setState({currentLeftCoords: "-90px 0px 0px -211px"});      
+                    }else{
+                        this.setState({currentLeftCoords: "0px 0px 0px -211px"});
+                    }
+                }else if(44 < Number(y)){
+                    this.setState({currentLeftCoords: "15px 0px 0px -40px"});
+                }else if(26 > Number(y)){
+                    this.setState({currentLeftCoords: "22px 0px 0px -22px"});
+                }else{
+                    this.setState({currentLeftCoords: "0px 0px 0px 0px"});
+                }
+            }else{
+                if(currentLeftCords < Number(y)){
+                    this.setState({currentLeftCoords: "0px 0px 0px -211px"});
+                }else{
+                    this.setState({currentLeftCoords: "0px 0px 0px 0px"});
+                }
+            }
+        }
       }
       
       componentWillUnmount() {
