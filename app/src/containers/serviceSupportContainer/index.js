91d3a2745c278c2e841568155cce3a6db2c9935b
mdsahir1990 <mdsahir@solveda.com>
2019-09-19 12:48:44 +0530

    side-bar-faq

--- app/src/containers/serviceSupportContainer/index.js
+++ app/src/containers/serviceSupportContainer/index.js
@@ -17,6 +17,19 @@ export default class HelpSupport extends React.Component {
     return (
       <div className='staticpage serviceSupport'>
 		<Breadcrumb {...this.props.match.params} staticName = {'Service & Support'}/>
+    <div className='container'>
+    <ul>
+      <li style={{color: '#eef8fe'}}><a href='/support'>My Orders</a></li>
+      <li><a href='/support'>Before I Buy</a></li>
+      <li><a href='/support'>Payments</a></li>
+      <li><a href='/support'>Warranty and Post Delivery Services</a></li>
+      <li><a href='/support'>My Order Queries</a></li>
+      <li><a href='/support'>Cancellations and Returns</a></li>
+      <li><a href='/support'>Gift Cards</a></li>
+      <li><a href='/support'>Request Service</a></li>
+    </ul>
+    </div>
+   
         <ContentEspot espotName={ 'GI_SUPPORT_FAQ' } />
         <a id='customerCare'>
        <ContentEspot espotName={ 'GI_CONTACT_US_CUSTOMER_CARE' } />
