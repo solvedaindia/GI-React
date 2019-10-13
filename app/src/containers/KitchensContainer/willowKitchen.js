cbd50a66c455c9a6e169afb141c7014d7769b216
dSahuSolveda <team@123455>
2019-10-13 22:43:10 +0530

    Willow Kitchen

--- app/src/containers/KitchensContainer/willowKitchen.js
+++ app/src/containers/KitchensContainer/willowKitchen.js
@@ -5,7 +5,10 @@ import ContentEspot from '../../components/Primitives/staticContent';
 import WillowKitchenBanner from '../../components/KitchensComp/willowKitchenbanner';
 import {Helmet} from "react-helmet";
 import Pixels from '../../components/Primitives/pixels';
-
+import ExKitchens from '../../components/KitchensComp/expKitchens';
+import {BOOK_CONSULTATION,KITCHEN_CONSULT} from '../../constants/app/primitivesConstants';
+import '../../../public/styles/static-pages/consultForm.scss';
+import ConsultationForm from '../../components/Primitives/ConsultForm';
 
 export default class WillowKitchens extends React.Component {
 constructor(props){
@@ -23,37 +26,36 @@ componentDidMount(){
 }
 
 render(){
-return (
-  <>
-  <ContentEspot espotName={ 'GI_PIXEL_WILLOW_KITCHEN_BODY_START' } />
-<div className="staticpage willowKitchen">
-<Helmet>
-	<Pixels espotName= {'GI_PIXEL_WILLOW_KITCHEN_META'}/>
-</Helmet>
-    <WillowKitchenBanner />
-  <ContentEspot espotName = { 'GI_MODULAR_WILLOW_KITCHEN_BENEFITS' } />
+	return (
+	  <>
+		<ContentEspot espotName={ 'GI_PIXEL_WILLOW_KITCHEN_BODY_START' } />
+		<div className="staticpage willowKitchen">
+			<Helmet>
+				<Pixels espotName= {'GI_PIXEL_WILLOW_KITCHEN_META'}/>
+			</Helmet>
+			<WillowKitchenBanner />
+			  <ContentEspot espotName = { 'GI_MODULAR_WILLOW_KITCHEN_BENEFITS' } />
+			  <ContentEspot espotName = { 'GI_MODULAR_KITCHEN_INTERIOR_WILLOW' } />
+			  <div className="formContainer">
+				  <ContentEspot espotName={ 'GI_KITCHEN_FORMBACKGROUND_IMG' } />
+				  <div id='consultForm' className="formDetails">
+						<h2 className="title">{BOOK_CONSULTATION}</h2>
+						<p className="desc">{KITCHEN_CONSULT} </p>
+						<ConsultationForm />
+				  </div>
+			  </div>
+			  <ContentEspot espotName={ 'GI_WILLOW_KITCHEN_FAQ' } />
+			  <ExKitchens />
 
-  <ContentEspot espotName = { 'GI_MODULAR_KITCHEN_INTERIOR_WILLOW' } />
-  <div className="formContainer">
-	  <ContentEspot espotName={ 'GI_KITCHEN_FORMBACKGROUND_IMG' } />
+			  <ContentEspot espotName={ 'GI_WILLOW_KITCHEN_INTERIOR' } />
 
-	  <div id='consultForm' className="formDetails">
-			<h2 className="title">{BOOK_CONSULTATION}</h2>
-			<p className="desc">{KITCHEN_CONSULT} </p>
-			<ConsultationForm />
-	  </div>
-  </div>
-  <ContentEspot espotName={ 'GI_WILLOW_KITCHEN_FAQ' } />
-  <ExKitchens />
-  <ContentEspot espotName={ 'GI_WILLOW_KITCHEN_INTERIOR } />
-  <ContentEspot espotName={ 'GI_MODULAR_KITCHEN_10' } />
-  <ContentEspot espotName={ 'GI_MODULAR_KITCHEN_11' } />
-
-</div>
-<ContentEspot espotName={ 'GI_PIXEL_WILLOW_KITCHEN_BODY_END' } />
-</>
+			  <ContentEspot espotName={ 'GI_MODULAR_KITCHEN_10' } />
+			  <ContentEspot espotName={ 'GI_MODULAR_KITCHEN_11' } />
+   		 </div>
+		<ContentEspot espotName={ 'GI_PIXEL_WILLOW_KITCHEN_BODY_END' } />
+		</>
 
 
-)
-}
+	)
+	}
 }
