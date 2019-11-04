82b6f743167046e9868b29ed4785b29bfcf2d43a
aahmad-solveda <aahmad@solveda.com>
2019-11-04 14:01:52 +0530

    Issue 1 and 2 fixed

--- app/src/components/HeaderComponent/headerL1/lightHeader.js
+++ app/src/components/HeaderComponent/headerL1/lightHeader.js
@@ -8,7 +8,8 @@ const LightHeader = () => (
   <div className="lighHeader">
     <figure className="logo">
       <a href="/">
-        {!isMobile() ? <WhiteLogo width="171" height="33" /> : '' }
+      {!isMobile() ? <WhiteLogo width="171" height="33" /> : 
+          <img className='logoImg' src={require('../../../../public/images/plpAssests/lazyloadingIndicatorWhite.svg')} alt="logo" /> }
       </a>
     </figure>
   </div>
