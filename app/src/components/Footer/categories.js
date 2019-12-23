63081e38ff5a9b2e7c4325a84a320c46ad6f722a
Deepak <dsahu@solveda.com>
2019-12-23 16:23:49 +0530

    Point 1 resolved

--- app/src/components/Footer/categories.js
+++ app/src/components/Footer/categories.js
@@ -1,19 +1,60 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+//Redux Imports
+import { connect } from 'react-redux';
+import injectSaga from '../../utils/injectSaga';
+import injectReducer from '../../utils/injectReducer';
+import reducer from '../../containers/PlpContainer/reducer';
+import saga from '../../containers/PlpContainer/saga';
+import { compose } from 'redux';
+import * as actionCreators from '../../containers/PlpContainer/actions';
+import {createCategoryPlpURL, getReleventReduxState, fetchReleventSortingValue, fetchReleventSortingValueByIndex } from '../../utils/utilityManager';
 
-const Categories = (props) => {
+import { Col } from 'react-bootstrap';
+import { Link, withRouter } from 'react-router-dom';
+
+// const Categories = (props) => {
+//     return(
+//         <Col md={12} sm={12} className='categories_section'>
+//             <div>
+//                 <h3 className='heading'>
+//                     {props.name.text}
+//                 </h3>
+//                 { props.name.children && props.name.children.length > 0 &&
+//                     props.name.children.map((links, i) => {
+//                     return (
+//                         <h4 className='categoriestext list' key={i}>
+//                             <Link className='link' to={links.action} /* onClick={() => console.log("Nav Link Footer -- ",link.action)} */>
+//                                 {links.text}
+//                             </Link>
+//                         </h4>
+//                     )})
+//                 }
+//             </div>
+//         </Col>
+//     );
+// }
+
+//export default Categories;
+
+
+class Categories extends React.Component {
+ 
+    onLinkNavigation = () => {
+        this.props.plpReduxStateReset();
+      }
+
+   render() {
     return(
         <Col md={12} sm={12} className='categories_section'>
             <div>
                 <h3 className='heading'>
-                    {props.name.text}
+                    {this.props.name.text}
                 </h3>
-                { props.name.children && props.name.children.length > 0 &&
-                    props.name.children.map((links, i) => {
+                { this.props.name.children && this.props.name.children.length > 0 &&
+                    this.props.name.children.map((links, i) => {
                     return (
                         <h4 className='categoriestext list' key={i}>
-                            <Link className='link' to={links.action}>
+                            <Link className='link' to={links.action} onClick={this.onLinkNavigation}>
                                 {links.text}
                             </Link>
                         </h4>
@@ -22,6 +63,38 @@ const Categories = (props) => {
             </div>
         </Col>
     );
+   }
 }
 
-export default Categories;
+
+/* ----------------------------------------   REDUX HANDLERS   -------------------------------------  */
+const mapDispatchToProps = dispatch => {
+    return {
+      plpReduxStateReset: () => dispatch(actionCreators.resetPLPReduxState()),
+    }
+  };
+  
+  const mapStateToProps = state => {
+    const stateObj = getReleventReduxState(state, 'plpContainer');
+    return {
+  
+    }
+  };
+  
+  const withConnect = connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  );
+  
+  const withReducer = injectReducer({ key: 'plpContainer', reducer });
+  const withSaga = injectSaga({ key: 'plpContainer', saga });
+  
+  export default compose(
+    withReducer,
+    withSaga,
+    withConnect,
+    withRouter,
+  )(Categories);
+
+// export default Categories;
+
