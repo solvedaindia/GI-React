35bb24f10f18e30a462dbe90a1b4581abb5b355a
Rohit Gupta <rgupta@salmonggn.com>
2020-01-08 11:27:02 +0530

    Fixed Intercom Issue

--- app/src/containers/FooterContainer/footer.js
+++ app/src/containers/FooterContainer/footer.js
@@ -51,13 +51,29 @@ class FooterContainer extends React.Component {
     if (userId == null || userId == '') {
       userId = appCookie.get('loginID');
     }
-    window.intercomSettings = {
-      app_id: "rf3dsabc",
-      name: userName, // Full name
-      email: userId, // Email address
-      created_at: `in ISO 8601: ${Date.now()}`
-    };
-    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rf3dsabc';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+
+    if(appCookie.get('isLoggedIn') === 'true'){
+      window.Intercom('boot',{
+        app_id: "rf3dsabc",
+        name: userName, // Full name
+        email: appCookie.get('loginID'), // Email address
+        created_at: `in ISO 8601: ${Date.now()}`,
+      });
+      (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rf3dsabc';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    } else {
+      window.Intercom('boot', {
+        app_id: 'rf3dsabc',
+        name: userName,
+        created_at: `in ISO 8601: ${Date.now()}`
+     });
+      (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rf3dsabc';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    }
+    // window.intercomSettings = {
+    //   app_id: "rf3dsabc",
+    //   name: userName, // Full name
+    //   // email: userId, // Email address
+    //   created_at: `in ISO 8601: ${Date.now()}`
+    // };
   }
 
   render() 
