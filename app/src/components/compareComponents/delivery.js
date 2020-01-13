6ebfb37a826ae713c3cf360be69b60092cf9da8e
aahmad-solveda <aahmad@solveda.com>
2020-01-14 00:55:50 +0530

    #3 compare page delivery date issue fixed

--- app/src/components/compareComponents/delivery.js
+++ app/src/components/compareComponents/delivery.js
@@ -7,10 +7,13 @@ import { Row, Col, Grid } from 'react-bootstrap';
 import close from '../../../public/images/close.svg';
 import Link from 'react-router-dom/Link';
 import appCookie from '../../utils/cookie';
+import apiManager from '../../utils/apiManager';
 import apiManger from '../../utils/apiManager';
+import { pdpApi2,pinCodeAPI, pinCodeAPIBundle} from '../../../public/constants/constants';
 import { storeId, accessToken, findinventoryAPI } from '../../../public/constants/constants';
 
 import axios from 'axios';
+import { fromJS } from 'immutable';
 
 class DelContainer extends React.Component {
   constructor(props) {
@@ -21,13 +24,109 @@ class DelContainer extends React.Component {
     }
   }
   componentDidMount() {
-    this.callDeliveryDateAPI(this.props.skuData.partNumber);
+
+    console.log("saaaaaaa",this.props.skuData);
+    if(this.props.skuData.type==="ItemBean")
+    {
+      this.callDeliveryDateAPI(this.props.skuData.partNumber);
+    }
+    else if(this.props.skuData.type==="BundleBean" || this.props.skuData.type==="KitBean")
+    {
+      apiManager.get(pdpApi2 + this.props.skuData.partNumber).then(response => {
+        console.log("alaaaaa",response.data)
+        let skuId =this.props.skuData.partNumber; 
+        if(response.data.data.type==='kit')
+        {
+          response.data.data.kitData.map(skuLevelData => {
+            if (skuId === skuLevelData.partNumber ||  skuId === skuLevelData.uniqueID) {
+              this.getActualResolvedData(response.data.data.kitData, skuLevelData, response.data.data.type);
+            }
+          });
+        }
+        else if(response.data.data.type==='bundle')
+        {
+          response.data.data.bundleData.map(skuLevelData => {
+            if (skuId === skuLevelData.partNumber ||  skuId === skuLevelData.uniqueID) {
+              this.getActualResolvedData(response.data.data.bundleData, skuLevelData, response.data.data.type);
+            }
+          });
+        }
+
+      }).catch(error => {
+      });
+
+    }
+    
+  }
+
+
+  getPincodeApiParams(resolvedSkuData, type) {
+		let partnumber = [];
+		let quantity = [];
+		let uniqueid = [];
+		let dataParams;
+		if (resolvedSkuData.itemInThisBundle && type === 'bundle') {
+			resolvedSkuData.itemInThisBundle.map((data) => {
+				partnumber.push(data.partNumber);
+				quantity.push(data.quantity);
+				uniqueid.push(data.uniqueID);
+				
+				 dataParams = {
+					params: {
+					partnumber: partnumber.toString(),
+					quantity: quantity.toString(),
+					uniqueid: uniqueid.toString(),
+					},
+				};
+			})
+		} else {
+			 dataParams = {
+				params: {
+				partnumber: resolvedSkuData.partNumber,
+				quantity: 1,
+				uniqueid: resolvedSkuData.uniqueID,
+				},
+			};
+		}
+
+		return dataParams;
+
+	}
+
+  /* get actual resolve data  */
+	getActualResolvedData(data, resolvedSkuData, type) {
+      //this.callPinCodeAPI(resolvedSkuData, type);
+      
+      let callPincodeApi;
+      if (type === 'bundle') {
+        callPincodeApi = pinCodeAPIBundle;
+      } else {
+        callPincodeApi = pinCodeAPI;
+      }
+      let pincodeVal = appCookie.get('pincode');
+      const dataParams = this.getPincodeApiParams(resolvedSkuData, type);
+
+      apiManager.get(callPincodeApi + pincodeVal, dataParams).then(response => {
+        console.log("deliveryData",response.data.data)
+        this.setState({
+          isLoading: false,
+          deliveryData: 'Found',
+        });
+      }).catch(error => {
+  
+        this.setState({
+          isLoading: false,
+          deliveryData:'Error'
+        });
+      });
+
   }
 
   callDeliveryDateAPI = (partNumber) => {
     var token = appCookie.get('accessToken');
     var defPin = appCookie.get('pincode');
     var url = `${findinventoryAPI}${defPin}?partNumber=${partNumber}&quantity=1`
+    
     axios.get(url, {
       headers: {
         store_id: storeId,
