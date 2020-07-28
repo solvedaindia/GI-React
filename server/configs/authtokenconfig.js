ebc0778b5a1f490b79f01b55a73018c4a9cb2e24
Deepak <dsahu@solveda.com>
2020-07-28 16:11:49 +0530

    Node code removed with Rohit

--- server/configs/authtokenconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports.getAuthConfig = function getAuthConfig() {
-  return {
-    encryptionKey: 'yqzSYsrLLYkJBya0P513QGqQq82CiojT',
-    encodingKey: 'rVlJvxsARa0bDTwFeSrnoQCO7SPN0lFt',
-  };
-};
