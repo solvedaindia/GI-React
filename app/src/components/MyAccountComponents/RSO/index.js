44e079a5492e5f2c4eace8e4d9986c3f799f692b
rkumar-solveda <59009446+rkumar-solveda@users.noreply.github.com>
2020-07-29 14:16:17 +0530

    fixes: RSO for web

---
+++ app/src/components/MyAccountComponents/RSO/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import RSOItem from './rsoItem'
+class RSODetail extends React.Component {
+
+
+
+    constructor(props) {
+        super(props);
+    }
+    onCancelPress()
+    {
+        this.props.backPress();
+    }
+    render()
+    {
+        console.log("orderData",this.props.orderData.returnOrder);
+        const data=this.props.orderData.returnOrder;
+        return(
+        <div className="trackMyOrder service-request">
+            <div className="bottomDivider">
+                <button className="backBtn" onClick={this.onCancelPress.bind(this)} >{`< RETURN DETAILS`}</button>
+            </div>
+            {
+                data && Array.isArray(data) &&
+                data.map(item=>{
+                    return (
+                        <RSOItem data={item}/>
+                    )
+                })
+            }
+
+        </div>
+        );
+    }
+
+
+}
+export default RSODetail;
