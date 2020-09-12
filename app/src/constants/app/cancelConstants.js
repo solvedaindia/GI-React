f7dca8cd6919437d8c740eb3cc67c85ddf2ef93d
rkumar-solveda <59009446+rkumar-solveda@users.noreply.github.com>
2020-09-12 15:31:41 +0530

    fixes: GIP-807

--- app/src/constants/app/cancelConstants.js
+++ app/src/constants/app/cancelConstants.js
@@ -1,34 +1,33 @@
-//  cancel constants 
+//  cancel constants
 
-
-export const MESSAGE_CANCEL = "We are sorry to hear that! Please let us know why you wish to cancel the order."; 
-export const CANCEL_ORDER = 'Cancel Order';
-export const CANCEL_ITEM = 'Cancel Item';
-export const MESSAGE_REFUND ="How do you want to process the refund?";
-export const MESSAGE_REFUND_METHOD ="Your refund will be processed to ";
-export const ERROR_MESSAGE_REASON ="Please chose your reason";
-export const PAYMENT_MODE ="Online Payment Method";
-export const CACELATION_WINDOW_CLOSE="Cancellation window is closed";
-export const CACELATION_RESAON_ERROR="";
+export const MESSAGE_CANCEL =
+  "We are sorry to hear that! Please let us know why you wish to cancel the order.";
+export const CANCEL_ORDER = "Cancel Order";
+export const CANCEL_ITEM = "Cancel Item";
+export const MESSAGE_REFUND = "How do you want to process the refund?";
+export const MESSAGE_REFUND_METHOD = "Your refund will be processed to ";
+export const ERROR_MESSAGE_REASON = "Please choose your reason";
+export const PAYMENT_MODE = "Online Payment Method";
+export const CACELATION_WINDOW_CLOSE = "Cancellation window is closed";
+export const CACELATION_RESAON_ERROR = "";
 
 export const DROPDOWN_OPTIONS_ITEM = [
-    'Placed order by mistake',
-    'Found better product',
+  "Placed order by mistake",
+  "Found better product"
 ];
 export const DROPDOWN_OPTIONS_COMMON = [
-    'Changed my mind',
-    'Purchased from retail store',
-    'Found better deal',
-    'Other'
+  "Changed my mind",
+  "Purchased from retail store",
+  "Found better deal",
+  "Other"
 ];
 export const DROPDOWN_OPTIONS_COMMON_ITEM = [
-    'Placed order by mistake',
-    'Changed my mind',
-    'Found better product',
-    'Purchased from retail store',
-    'Found better deal',
-    'Other'
+  "Placed order by mistake",
+  "Changed my mind",
+  "Found better product",
+  "Purchased from retail store",
+  "Found better deal",
+  "Other"
 ];
 
-export const MESSAGE_TEXTBOX = 'Specify reason for other option'
-
+export const MESSAGE_TEXTBOX = "Specify reason for other option";
